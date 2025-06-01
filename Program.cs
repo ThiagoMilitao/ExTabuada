239fe9acef394aaf707082a88fed4bee5db99357
thiagoMilitao <thiago.militao2007@gmail.com>
2025-05-31 23:35:42 -0300

    tabuada

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("=======Tabuada======");
+Console.Write("Digite um número inteiro: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Tabuado do número {num}");
+for (int i = 0; i <= 10; i++)
+{
+    Console.WriteLine($"{num} x {i} = {num * i}");
+}
